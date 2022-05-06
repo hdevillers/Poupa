1aa6c3fec1c76478049340652eb90fba988d4137
Maëlis-Rakowski <maelis.rakowski@etu.umontpellier.fr>
2022-05-06 14:32:25 +0200

    home reçoit directement les objets Experience et Capteur

--- pages/home.py
+++ pages/home.py
@@ -12,18 +12,16 @@ class HomePage(HydraHeadApp):
     def run(self):
         # st.header("Bienvenu " + st.session_state['prenom_nom'])
         # all_exp = models.get_by("experiences", "operateur", st.session_state['login'])
-        all_exp = models.get_by("experiences", 'operateur', st.session_state['login'])
+        all_exp = models.Experience.get_experiences('operateur', st.session_state['login'])
         for experience in all_exp:
             with st.container():
-                st.subheader(experience[0])
-                list_cpt = models.get_by("capteurs", "id_experience", experience[0])
+                st.subheader(experience.get_id())
+                list_cpt = models.Capteur.get_capteurs("id_experience", experience.get_id())
                 col1, col2 = st.columns(2)
                 col3, col4 = st.columns(2)
                 list_cols = [col1, col2, col3, col4]
                 i = 0
                 for capteur in list_cpt:
                     with list_cols[i]:
-                        st.write(f"**{str(capteur[0])}**")
-                        st.write(f"farine utilisé = {capteur[2]}")
-                        st.write(f"levain utilisé = {capteur[3]}")
+                        st.write(str(capteur))
                     i += 1
