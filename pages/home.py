0e9cb739d8171c20042b3d6f6ad01a3a2880d88f
Maëlis-Rakowski <maelis.rakowski@etu.umontpellier.fr>
2022-05-23 16:57:03 +0200

    généralisation en fonction de la façon d'afficher chaque experiences et ses capteurs

--- pages/home.py
+++ pages/home.py
@@ -14,16 +14,7 @@ class HomePage(HydraHeadApp):
             all_exp = models.Experience.get_experiences('operateur', st.session_state['login'])
             for experience in all_exp:
                 with st.container():
-                    st.subheader(experience.get_id())
-                    list_cpt = models.Capteur.get_capteurs("id_experience", experience.get_id())
-                    col1, col2 = st.columns(2)
-                    col3, col4 = st.columns(2)
-                    list_cols = [col1, col2, col3, col4]
-                    i = 0
-                    for capteur in list_cpt:
-                        with list_cols[i]:
-                            st.write(str(capteur))
-                        i += 1
+                    experience.str_exp_and_cpt()
         else:
             st.warning(f"Vous êtes connecté(e) en **mode visiteur**, vous n'avez donc **pas accés** à la base de "
                        f"données, les farines, levains et levures que vous créérez **seront perdus** si vous vous "
