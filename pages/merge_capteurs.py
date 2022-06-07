1c898cfd2a9d002d4748bc212e06f22e983efe41
Maëlis-Rakowski <maelis.rakowski@etu.umontpellier.fr>
2022-06-07 08:45:54 +0200

    merge capteur avec les fichiers uniquement

--- pages/merge_capteurs.py
+++ pages/merge_capteurs.py
@@ -9,9 +9,13 @@ class MergePage(HydraHeadApp):
         self.title = title
 
     def run(self):
+        st.header("Fusion de Pâtons")
+        st.write("Sur cette page comparez plusieurs pâtons d'experience différentes. Pour cela, vous devez d'abord "
+                 "avoir généré l'experience du pâton voulu et enregistré les résultats sur votre ordinateur. Vous "
+                 "pouvez alors selectionner le fichier des capteurs correspondants dans le selecteur ci-dessous ")
         with st.form('choose cpt'):
             list_cpt = []
-            if self.session_state.allow_access > 1:
+            """if self.session_state.allow_access > 1:
                 all_exp = models.get_by("experiences", 'operateur', st.session_state['login'])
                 list_checkbox = []
                 i = 0
@@ -23,19 +27,18 @@ class MergePage(HydraHeadApp):
                         for capteur in list_cpt:
                             cpt_checkbox = st.checkbox(str(capteur), key=f"chckbx{i}")
                             list_checkbox.append(cpt_checkbox)
-                            i += 1
-            else:
-                list_cpt = st.file_uploader("Fichier des flacons à fusionner", accept_multiple_files=True)
+                            i += 1"""
+
+            list_cpt = st.file_uploader("Fichier des flacons à fusionner", accept_multiple_files=True)
             if st.form_submit_button("Fusionner"):
                 i = 0
                 list_file = []
-                if self.session_state.allow_access > 1:
+                """if self.session_state.allow_access > 1:
                     for cpt in list_cpt:
                         if list_checkbox[i]:
                             list_file.append(cpt.get_fichier_donnes())
-                        i += 1
-                else:
-                    list_file = list_cpt
+                        i += 1"""
+                list_file = list_cpt
 
                 if not list_file or len(list_file) == 1:
                     st.error("Veuillez choisir au moins 2 capteurs")
